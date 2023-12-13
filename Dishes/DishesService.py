1f0dec2474f0f51fc3b80fee1df3c5f3b42ca142
rodionkasirin <k.rodion77@gmail.com>
2023-12-13 15:07:19 +0300

    third commit

--- Dishes/DishesService.py
+++ Dishes/DishesService.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 #dishes-services
+#
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rodionkasirin:12345@postgres:5432/items_db'
